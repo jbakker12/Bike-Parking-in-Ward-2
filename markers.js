20806e64795c98abdf0dce2c50bcef2d57196c6d
jdb-123 <jbakker12@proton.me>
2025-02-17 14:07:13 -0500

    Formatted markers to be in proper json format

--- markers.js
+++ markers.js
@@ -1,48 +1,303 @@
-X,Y,LOCATION_NAME,ADDRESS,TOTAL_CAPACITY,COVERED,NEARBY_LIGHTING
 
 const bikeRacks = [
-
-{592452.878,4789812.141,"Goodwill Career Centre","225 King William St",6,"No","Yes","No"}
-{592311.5543,4789781.594,"Social Planning and Research Council of Hamilton" ,"Walnut and King William",6,"No","Yes","No"}
-{591728.8701,4790222.108,"Coppley Noyes & Randall Wholesale Clothing" ,"56 York Blvd, Hamilton",6,"No","Yes","No"}
-{591536.9567,4790287.234,"Salvation Army", "101 York Boulevard, Hamilton",10,"No","Yes","No"}
-{591909.0615,4789021.692,"Woolverton Park","90 Charlton Avenue East",10,"No","Yes","No"}
-{592438.9411,4791490.19,"Bennetto Community Centre","450 Hughson Street North",8,"No","Yes","No"}
-{591609.7685,4791302.6,"Bayfront Park","200 Harbour Front Drive",10,"No","Yes","No"}
-{592417.6191,4790016.595,"Beasley Community Centre","145 Wilson Street, Hamilton",14,"No","Yes","No"}
-{592589.0898,4789170.762,"Central Memorial Recreation Centre","93 West Avenue South, Hamilton",14,"No","No","No"}
-{592597.7756,4789188.763,"Central Memorial Recreation Centre","93 West Avenue South, Hamilton",8,"No","Yes","No"}
-{592542.4951,4789074.713,"Carter Park","32 Stinson Street",16,"No","Yes","No"}
-{591731.8068,4789581.671,"GO Station","36 Hunter Street East",28,"Yes","Yes","No"}
-{592173.9563,4791866.677,"The Royal Hamilton Yacht Club","bay n",10,"No","No","No"}
-{592010.6526,4790002.551,"Secure Facility - Fire Station No. 1 - Fire Station Employees only","33 John Street North",8,"Yes","Yes","No"}
-{592340.8505,4791779.427,"Hamilton Port Authority","605 James St N",14,"No","No","No"}
-{592046.1996,4790997.502,"West Harbour GO Station","353 James Street N., Hamilton, ON",28,"Yes","Yes","No"}
-{592051.8886,4790991.462,"West Harbour GO Station","353 James Street N., Hamilton, ON",28,"Yes","Yes","No"}
-{591853.0945,4789547.77,"Hamilton GO Centre","36 Hunter St. E",28,"Yes","Yes","No"}
-{591452.8571,4789723.786,"City Hall Secure Bike Parking Facility","71 Main Street West",31,"Yes","Yes","No"}
-{591767.9172,4790065.357,"Jackson Square East Parking Garage Secure Bike Parking","2 King Street West",25,"Yes","Yes","No"}
-{592092.2754,4789694.972,"Effort Trust","105 Main St E",4,"Yes","Yes","No"}
-{592102.3214,4789669.378,"Landmark Place","100 Main Street East",4,"No","No","No"}
-{591798.253,4790277.255,"York Boulevard Parkade - Secure Bike Cage","28 York Boulevard",43,"Yes","Yes","Yes"}
-{592012.2038,4790059.061,"Horizon Utilities - Secure Bike Parking","55 John Street",10,"Yes","Yes","Yes"}
-{591870.5222,4790053.67,"Lister Block - Secure Bike Parking","28 James Street North",20,"Yes","Yes","Yes"}
-{591529.3093,4789956.844,"Summer's Lane - Secure Bike Parking","80 Summer's Lane",16,"Yes","Yes","Yes"}
-{591429.0657,4790237.339,"Government of Canada - Secure Bike Parking","55 Bay Street",20,"Yes","Yes","Yes"}
-{591549.8865,4788978.222,"St. Joseph's Hospital - For St. Joe's Employees ONLY","50 Charlton Avenue East",12,"Yes","Yes","No"}
-{592010.4378,4791813.28,"Leander Boat Club","50 Leander Drive",12,"No","No","No"}
-{592818.369,4791673.133,"Eastwood Arena & Park","111 Burlington Street East",12,"Yes","Yes","No"}
-
+    {
+        "X": 592452.878,
+        "Y": 4789812.141,
+        "LOCATION_NAME": "Goodwill Career Centre",
+        "ADDRESS": "225 King William St",
+        "TOTAL_CAPACITY": 6,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592311.5543,
+        "Y": 4789781.594,
+        "LOCATION_NAME": "Social Planning and Research Council of Hamilton",
+        "ADDRESS": "Walnut and King William",
+        "TOTAL_CAPACITY": 6,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591728.8701,
+        "Y": 4790222.108,
+        "LOCATION_NAME": "Coppley Noyes & Randall Wholesale Clothing",
+        "ADDRESS": "56 York Blvd, Hamilton",
+        "TOTAL_CAPACITY": 6,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591536.9567,
+        "Y": 4790287.234,
+        "LOCATION_NAME": "Salvation Army",
+        "ADDRESS": "101 York Boulevard, Hamilton",
+        "TOTAL_CAPACITY": 10,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591909.0615,
+        "Y": 4789021.692,
+        "LOCATION_NAME": "Woolverton Park",
+        "ADDRESS": "90 Charlton Avenue East",
+        "TOTAL_CAPACITY": 10,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592438.9411,
+        "Y": 4791490.19,
+        "LOCATION_NAME": "Bennetto Community Centre",
+        "ADDRESS": "450 Hughson Street North",
+        "TOTAL_CAPACITY": 8,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591609.7685,
+        "Y": 4791302.6,
+        "LOCATION_NAME": "Bayfront Park",
+        "ADDRESS": "200 Harbour Front Drive",
+        "TOTAL_CAPACITY": 10,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592417.6191,
+        "Y": 4790016.595,
+        "LOCATION_NAME": "Beasley Community Centre",
+        "ADDRESS": "145 Wilson Street, Hamilton",
+        "TOTAL_CAPACITY": 14,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592589.0898,
+        "Y": 4789170.762,
+        "LOCATION_NAME": "Central Memorial Recreation Centre",
+        "ADDRESS": "93 West Avenue South, Hamilton",
+        "TOTAL_CAPACITY": 14,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "No",
+        "SECURED": "No"
+    },
+    {
+        "X": 592597.7756,
+        "Y": 4789188.763,
+        "LOCATION_NAME": "Central Memorial Recreation Centre",
+        "ADDRESS": "93 West Avenue South, Hamilton",
+        "TOTAL_CAPACITY": 8,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592542.4951,
+        "Y": 4789074.713,
+        "LOCATION_NAME": "Carter Park",
+        "ADDRESS": "32 Stinson Street",
+        "TOTAL_CAPACITY": 16,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591731.8068,
+        "Y": 4789581.671,
+        "LOCATION_NAME": "GO Station",
+        "ADDRESS": "36 Hunter Street East",
+        "TOTAL_CAPACITY": 28,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592173.9563,
+        "Y": 4791866.677,
+        "LOCATION_NAME": "The Royal Hamilton Yacht Club",
+        "ADDRESS": "bay n",
+        "TOTAL_CAPACITY": 10,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "No",
+        "SECURED": "No"
+    },
+    {
+        "X": 592010.6526,
+        "Y": 4790002.551,
+        "LOCATION_NAME": "Secure Facility - Fire Station No. 1 - Fire Station Employees only",
+        "ADDRESS": "33 John Street North",
+        "TOTAL_CAPACITY": 8,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592340.8505,
+        "Y": 4791779.427,
+        "LOCATION_NAME": "Hamilton Port Authority",
+        "ADDRESS": "605 James St N",
+        "TOTAL_CAPACITY": 14,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "No",
+        "SECURED": "No"
+    },
+    {
+        "X": 592046.1996,
+        "Y": 4790997.502,
+        "LOCATION_NAME": "West Harbour GO Station",
+        "ADDRESS": "353 James Street N., Hamilton, ON",
+        "TOTAL_CAPACITY": 28,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592051.8886,
+        "Y": 4790991.462,
+        "LOCATION_NAME": "West Harbour GO Station",
+        "ADDRESS": "353 James Street N., Hamilton, ON",
+        "TOTAL_CAPACITY": 28,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591853.0945,
+        "Y": 4789547.77,
+        "LOCATION_NAME": "Hamilton GO Centre",
+        "ADDRESS": "36 Hunter St. E",
+        "TOTAL_CAPACITY": 28,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591452.8571,
+        "Y": 4789723.786,
+        "LOCATION_NAME": "City Hall Secure Bike Parking Facility",
+        "ADDRESS": "71 Main Street West",
+        "TOTAL_CAPACITY": 31,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 591767.9172,
+        "Y": 4790065.357,
+        "LOCATION_NAME": "Jackson Square East Parking Garage Secure Bike Parking",
+        "ADDRESS": "2 King Street West",
+        "TOTAL_CAPACITY": 25,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592092.2754,
+        "Y": 4789694.972,
+        "LOCATION_NAME": "Effort Trust",
+        "ADDRESS": "105 Main St E",
+        "TOTAL_CAPACITY": 4,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592102.3214,
+        "Y": 4789669.378,
+        "LOCATION_NAME": "Landmark Place",
+        "ADDRESS": "100 Main Street East",
+        "TOTAL_CAPACITY": 4,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "No",
+        "SECURED": "No"
+    },
+    {
+        "X": 591798.253,
+        "Y": 4790277.255,
+        "LOCATION_NAME": "York Boulevard Parkade - Secure Bike Cage",
+        "ADDRESS": "28 York Boulevard",
+        "TOTAL_CAPACITY": 43,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "Yes"
+    },
+    {
+        "X": 592012.2038,
+        "Y": 4790059.061,
+        "LOCATION_NAME": "Horizon Utilities - Secure Bike Parking",
+        "ADDRESS": "55 John Street",
+        "TOTAL_CAPACITY": 10,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "Yes"
+    },
+    {
+        "X": 591870.5222,
+        "Y": 4790053.67,
+        "LOCATION_NAME": "Lister Block - Secure Bike Parking",
+        "ADDRESS": "28 James Street North",
+        "TOTAL_CAPACITY": 20,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "Yes"
+    },
+    {
+        "X": 591529.3093,
+        "Y": 4789956.844,
+        "LOCATION_NAME": "Summer's Lane - Secure Bike Parking",
+        "ADDRESS": "80 Summer's Lane",
+        "TOTAL_CAPACITY": 16,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "Yes"
+    },
+    {
+        "X": 591429.0657,
+        "Y": 4790237.339,
+        "LOCATION_NAME": "Government of Canada - Secure Bike Parking",
+        "ADDRESS": "55 Bay Street",
+        "TOTAL_CAPACITY": 20,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "Yes"
+    },
+    {
+        "X": 591549.8865,
+        "Y": 4788978.222,
+        "LOCATION_NAME": "St. Joseph's Hospital - For St. Joe's Employees ONLY",
+        "ADDRESS": "50 Charlton Avenue East",
+        "TOTAL_CAPACITY": 12,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    },
+    {
+        "X": 592010.4378,
+        "Y": 4791813.28,
+        "LOCATION_NAME": "Leander Boat Club",
+        "ADDRESS": "50 Leander Drive",
+        "TOTAL_CAPACITY": 12,
+        "COVERED": "No",
+        "NEARBY_LIGHTING": "No",
+        "SECURED": "No"
+    },
+    {
+        "X": 592818.369,
+        "Y": 4791673.133,
+        "LOCATION_NAME": "Eastwood Arena & Park",
+        "ADDRESS": "111 Burlington Street East",
+        "TOTAL_CAPACITY": 12,
+        "COVERED": "Yes",
+        "NEARBY_LIGHTING": "Yes",
+        "SECURED": "No"
+    }   
 ]
-
-//have the sub arrays made into associative arrays
-for (let i=0; i<bikeRacks.length;i++){
-    bikeRacks[i][0] = "X";
-    bikeRacks[i][1] = "Y";
-    bikeRacks[i][2] = "LOCATION_NAME";
-    bikeRacks[i][3] = ",ADDRESS,";
-    bikeRacks[i][4] = "TOTAL_CAPACITY,";
-    bikeRacks[i][5] = "COVERED,";
-    bikeRacks[i][6] = "NEARBY_LIGHTING";
-    bikeRacks[i][7] = "SECURED";
-}
